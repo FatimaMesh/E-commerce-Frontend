c80e83e0f58bc3a0eb266b4000cf3759995ebb04
FatimaMesh <fatimah1011f@gmail.com>
2024-05-13 11:09:42 +0300

    link backend/frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import { useQuery } from "@tanstack/react-query"
-
 import { Button } from "./components/ui/button"
 import {
   Card,
@@ -13,23 +11,25 @@ import { Product } from "./types"
 import api from "./api"
 
 import "./App.css"
+import { useEffect, useState } from "react"
 
 function App() {
+  const [data, setData] = useState<Product[] | null>(null)
+  const [error, setError] = useState<string | null>(null)
   const getProducts = async () => {
     try {
       const res = await api.get("/products")
-      return res.data
+      console.log(res.data.data)
+      setData(res.data.data.$values)
+      setError(null)
     } catch (error) {
       console.error(error)
-      return Promise.reject(new Error("Something went wrong"))
+      setError("Something went wrong")
     }
   }
-
-  // Queries
-  const { data, error } = useQuery<Product[]>({
-    queryKey: ["products"],
-    queryFn: getProducts
-  })
+  useEffect(() => {
+    getProducts()
+  }, [])
 
   return (
     <div className="App">
@@ -37,13 +37,13 @@ function App() {
 
       <section className="flex flex-col md:flex-row gap-4 justify-between max-w-6xl mx-auto">
         {data?.map((product) => (
-          <Card key={product.id} className="w-[350px]">
+          <Card key={product.productId} className="w-[350px]">
             <CardHeader>
               <CardTitle>{product.name}</CardTitle>
-              <CardDescription>Some Description here</CardDescription>
+              <CardDescription>{product.description}</CardDescription>
             </CardHeader>
             <CardContent>
-              <p>Card Content Here</p>
+              <p>{product.price}</p>
             </CardContent>
             <CardFooter>
               <Button className="w-full">Add to cart</Button>
@@ -51,7 +51,7 @@ function App() {
           </Card>
         ))}
       </section>
-      {error && <p className="text-red-500">{error.message}</p>}
+      {error && <p className="text-red-500">{error}</p>}
     </div>
   )
 }
