aafaace524c39284afa3fba7172e31f8e704d6af
FatimaMesh <fatimah1011f@gmail.com>
2024-05-27 13:29:07 +0300

    update api file

--- src/api/index.ts
+++ src/api/index.ts
@@ -13,13 +13,13 @@ const api = axios.create({
   baseURL
 })
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token")
-  if (token) {
-    config.headers["Authorization"] = `Bearer.${token}`
-  }
-  return config
-})
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token")
+//   if (token) {
+//     config.headers["Authorization"] = `Bearer.${token}`
+//   }
+//   return config
+// })
 
 // use this to handle errors gracefully
 // api.interceptors.response.use(
